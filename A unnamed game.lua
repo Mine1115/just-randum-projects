9e688cd84d0a20de937b618a9ac19e445ab099bd
Mine1115 <126138054+Mine1115@users.noreply.github.com>
2023-04-20 14:43:26 -0500

    Update A unnamed game.lua

--- A unnamed game.lua
+++ A unnamed game.lua
@@ -1,25 +1,5 @@
-WOOD = 10
-STONE = 5
-POPULATION = 5
-FOOD = 10
-WATER = 10
-ENERGY = 5
-print("you are in charge of a village and you need to ckeep it up and runinh"); 
-
-print("wood",WOOD)
-print("stone",STONE)
-print("population",POPULATION)
-print("food",FOOD)
-print("water",WATER)
-print("Energy", ENERGY)
-print("(A) get food, (B) get water, (C) get stone and wood")
-ans = io.read()
- if ans == "a" then
-  FOOD = FOOD + 3
-  ENERGY = ENERGY - 1
-  print("food",FOOD)
-  print("Energy", ENERGY)
- end
+WOOD = 10; STONE = 5; POPULATION = 5; FOOD = 10; WATER = 10; ENERGY = 5; print("you are in charge of a village and you need to ckeep it up and runinh"); print("wood",WOOD); print("stone",STONE); print("population",POPULATION); print("food",FOOD); print("water",WATER);
+print("Energy", ENERGY); print("(A) get food, (B) get water, (C) get stone and wood"); ans = io.read(); if ans == "a" then FOOD = FOOD + 3; ENERGY = ENERGY - 1; print("food",FOOD); print("Energy", ENERGY) end
 if ans == "b" then
   WATER = WATER + 3
   ENERGY = ENERGY - 1
