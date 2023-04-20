f962b8bf9ba890c7ce22d319505579886cf40886
Mine1115 <126138054+Mine1115@users.noreply.github.com>
2023-04-20 12:06:21 -0500

    Update A unnamed game.lua

--- A unnamed game.lua
+++ A unnamed game.lua
@@ -1,33 +1,47 @@
-wood = 10
-stone = 5
-population = 5
-food = 10
-water = 10
-energy = 5
+WOOD = 10
+STONE = 5
+POPULATION = 5
+FOOD = 10
+WATER = 10
+ENERGY = 5
 print("you are in charge of a village and you need to ckeep it up and runinh"); 
 
-print("wood",wood)
-print("stone",stone)
-print("population",population)
-print("food",food)
-print("water",water)
-print("Energy", energy)
+print("wood",WOOD)
+print("stone",STONE)
+print("population",POPULATION)
+print("food",FOOD)
+print("water",WATER)
+print("Energy", ENERGY)
 print("(A) get food, (B) get water, (C) get stone and wood")
 ans = io.read()
  if ans == "a" then
-  food = food + 3
-  energy = energy - 1
-  print("food",food)
-  print("Energy", energy)
+  FOOD = FOOD + 3
+  ENERGY = ENERGY - 1
+  print("food",FOOD)
+  print("Energy", ENERGY)
  end
 if ans == "b" then
-  water = water + 3
-  energy = energy - 1
-  print("water",water)
-  print("Energy", energy)
+  WATER = WATER + 3
+  ENERGY = ENERGY - 1
+  print("water",WATER)
+  print("Energy", ENERGY)
  end
 if ans == "c" then
-  wood = wood + 3
+  WOOD = WOOD  = 10 
+ STONE  = 5 
+ ("water",water) 
+   print("Energy", energy) 
+  end 
+ if ans == "c" then 
+   WOOD  = WOOD + 3 
+   stone = stone + 3 
+   energy = energy - 1 
+   print("wood",WOOD, "stone", stone) 
+   print("Energy", energy) 
+  end 
+ if ans ~= "b"  and ~= "a" and ~= "c" then 
+   print("(A) get food, (B) get water, (C) get 
+  end + 3
   stone = stone + 3
   energy = energy - 1
   print("wood",wood, "stone", stone)
