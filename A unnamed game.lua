779a544490390c035487736ff2d0922b5cb5c14c
Mine1115 <126138054+Mine1115@users.noreply.github.com>
2023-04-21 15:12:13 -0500

    Update A unnamed game.lua

--- A unnamed game.lua
+++ A unnamed game.lua
@@ -1,1 +1,12 @@
+GAME = 1
+while GAME == 1 do
+  print("E E E E E E")
+  print("E E E E E E")
+  print("E E E E E E")
+  print("E E E E E E")
+  print("E E E E E E")
+  print("E E E E E E")
+  ANS = io.read()
+  print(ANS)
+end
 
